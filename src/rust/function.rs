26fd096cfaa1807f76d0fce0df91654c598716cb
Frank Denis <github@pureftpd.org>
2021-04-06 17:43:56 +0200

    More ergonomic returns for Rust

--- src/rust/function.rs
+++ src/rust/function.rs
@@ -51,16 +51,27 @@ impl RustGenerator {
             let name = "result_ptr";
             results.push((name.to_string(), ok_type));
         }
+        let mut results_decomposed = vec![];
         for result in &results {
             let mut decomposed = result.1.decompose(&result.0, true);
-            params_decomposed.append(&mut decomposed);
+            results_decomposed.append(&mut decomposed);
         }
 
-        Self::define_func_raw(w, module_name, &name, &params_decomposed, &result)?;
+        Self::define_func_raw(
+            w,
+            module_name,
+            &name,
+            &params_decomposed,
+            &results_decomposed,
+            &result,
+        )?;
 
         let signature_witx = func_witx.wasm_signature(witx::CallMode::DefinedImport);
         let params_count_witx = signature_witx.params.len() + signature_witx.results.len();
-        assert_eq!(params_count_witx, params_decomposed.len() + 1);
+        assert_eq!(
+            params_count_witx,
+            params_decomposed.len() + results_decomposed.len() + 1
+        );
 
         Ok(())
     }
@@ -70,8 +81,28 @@ impl RustGenerator {
         module_name: &str,
         name: &str,
         params_decomposed: &[ASTypeDecomposed],
+        results_decomposed: &[ASTypeDecomposed],
         result: &ASResult,
     ) -> Result<(), Error> {
+        let results_decomposed_deref = results_decomposed
+            .iter()
+            .map(|result_ptr_type| match result_ptr_type.type_.as_ref() {
+                ASType::MutPtr(result_type) => ASTypeDecomposed {
+                    name: result_ptr_type.name.clone(),
+                    type_: result_type.clone(),
+                },
+                _ => panic!("Result type is not a pointer"),
+            })
+            .collect::<Vec<_>>();
+        let results_set = results_decomposed_deref
+            .iter()
+            .map(|result| result.type_.as_lang())
+            .collect::<Vec<_>>();
+        let rust_fn_result_str = match results_set.len() {
+            0 => "()".to_string(),
+            1 => results_set[0].clone(),
+            _ => format!("({})", results_set.join(", ")),
+        };
         w.indent()?.write(format!("pub fn {}(", name.as_fn()))?;
         if !params_decomposed.is_empty() {
             w.eol()?;
@@ -83,8 +114,7 @@ impl RustGenerator {
                 param.type_.as_lang(),
             ))?;
         }
-        w.write_line(") -> Result<(), Error> {")?;
-
+        w.write_line(format!(") -> Result<{}, Error> {{", rust_fn_result_str))?;
         {
             let mut w = w.new_block();
 
@@ -98,7 +128,7 @@ impl RustGenerator {
                     if !params_decomposed.is_empty() {
                         w.eol()?;
                     }
-                    for param in params_decomposed {
+                    for param in params_decomposed.iter().chain(results_decomposed.iter()) {
                         w.write_line_continued(format!(
                             "{}: {},",
                             param.name.as_var(),
@@ -111,18 +141,43 @@ impl RustGenerator {
             }
 
             // Wrapper
+            for result in &results_decomposed_deref {
+                w.write_line(format!(
+                    "let mut {} = std::mem::MaybeUninit::uninit();",
+                    result.name.as_var()
+                ))?;
+            }
+
             w.write_line(format!("let res = unsafe {{ {}(", name.as_fn()))?;
             for param in params_decomposed {
                 w.write_line_continued(format!("{},", param.name.as_var()))?;
+            }
+            for result in results_decomposed_deref.iter() {
+                w.write_line_continued(format!("{}.as_mut_ptr(),", result.name.as_var()))?;
             }
             w.write_line(")};")?;
             w.write_lines(
                 "if res != 0 {
     return Err(Error::WasiError(res as _));
-}
-Ok(())",
+}",
             )?;
-        }
+            let res_str = match results_decomposed.len() {
+                0 => "()".to_string(),
+                1 => format!(
+                    "unsafe {{ {}.assume_init() }}",
+                    results_decomposed_deref[0].name.as_var()
+                ),
+                _ => format!(
+                    "unsafe {{ ({}) }}",
+                    results_decomposed_deref
+                        .iter()
+                        .map(|result| format!("{}.assume_init()", result.name.as_var()))
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                ),
+            };
+            w.write_line(format!("Ok({})", res_str))?;
+        };
         w.write_line("}")?;
         w.eob()?;
 
