c33b6960b486b1a2c286afc067c8ef0b135bde26
Frank Denis <github@pureftpd.org>
2021-04-08 16:36:11 +0200

    markdown: no need to escape too much

--- src/doc/mod.rs
+++ src/doc/mod.rs
@@ -47,20 +47,20 @@ impl<T: Write> Generator<T> for DocGenerator {
 
         w.write_line("## Table of contents")?.eob()?;
         w.write_line("### Types list:")?.eob()?;
-        w.write("\\[**[All](#types)**\\]")?;
+        w.write("[**[All](#types)**]")?;
         for type_ in module_witx.typenames() {
             if skip_imports && &type_.module != module_id {
                 continue;
             }
             let type_name = type_.name.as_str();
-            w.write(format!(" - \\[{}\\]", type_name.as_type()))?;
+            w.write(format!(" - [{}]", type_name.as_type()))?;
         }
         w.eol()?.eob()?;
         w.write_line("### Functions list:")?.eob()?;
-        w.write("\\[**[All](#functions)**\\]")?;
+        w.write("[**[All](#functions)**]")?;
         for func in module_witx.funcs() {
             let func_name = func.name.as_str();
-            w.write(format!(" - \\[{}\\]", func_name.as_fn()))?;
+            w.write(format!(" - [{}]", func_name.as_fn()))?;
         }
         w.eol()?.eob()?;
 
